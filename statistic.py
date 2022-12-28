392e357bcc351c38b0453c81dd5150ee06d129dc
dranh <dranh.club@gmail.com>
2022-12-29 00:02:53 +0700

    Delta dice curve

--- statistic.py
+++ statistic.py
@@ -2,115 +2,193 @@
 
 import cv2
 import matplotlib.pyplot as plt
-from utils import dice, get_test_raw_dataset, get_train_raw_dataset, RawResult
+from utils import dice, get_test_raw_dataset, RawResult
 from visualize import Visualizer
 from utils import DS_NAMES
 import argparse
 import numpy as np
 
-def dice_vs_size():
-    result = {
-        'CVC-300': {
-            'size': [],
-            'dice': []
-        }, 
-        'CVC-ClinicDB': {
-            'size': [],
-            'dice': []
-        }, 
-        'Kvasir': {
-            'size': [],
-            'dice': []
-        }, 
-        'CVC-ColonDB': {
-            'size': [],
-            'dice': []
-        }, 
-        'ETIS-LaribPolypDB': {
-            'size': [],
-            'dice': []
-        },
-    }
+class Statistic():
+    def __init__(self, name) -> None:
+        self.name = name
+        self.test_raw_dataset = get_test_raw_dataset()
+        self.raw_result = RawResult(name)
 
-    # For each dataset
-    for ds_name in DS_NAMES:
-        print("Processing", ds_name)
+    @staticmethod
+    def dice_vs_size(name):
+        result = {
+            'CVC-300': {
+                'size': [],
+                'dice': []
+            }, 
+            'CVC-ClinicDB': {
+                'size': [],
+                'dice': []
+            }, 
+            'Kvasir': {
+                'size': [],
+                'dice': []
+            }, 
+            'CVC-ColonDB': {
+                'size': [],
+                'dice': []
+            }, 
+            'ETIS-LaribPolypDB': {
+                'size': [],
+                'dice': []
+            },
+        }
 
-        test_raw_dataset = get_test_raw_dataset()
-        raw_result = RawResult(NAME)
+        # For each dataset
+        for ds_name in DS_NAMES:
+            print("Processing", ds_name)
 
-        # For each img
-        for img_idx in range(len(test_raw_dataset.filenames[ds_name])):
+            test_raw_dataset = get_test_raw_dataset()
+            raw_result = RawResult(name)
 
-            # Read GT, pred
-            gt = test_raw_dataset.get_gt(ds_name, img_idx)
-            pred = raw_result.get_result(ds_name, img_idx)
+            # For each img
+            for img_idx in range(len(test_raw_dataset.filenames[ds_name])):
 
-            # Percentage of white pixel per total number of pixel
-            percent = gt[gt > 0].size / gt.size * 100
-            result[ds_name]['size'].append(percent)
+                # Read GT, pred
+                gt = test_raw_dataset.get_gt(ds_name, img_idx)
+                pred = raw_result.get_result(ds_name, img_idx)
+
+                # Percentage of white pixel per total number of pixel
+                percent = gt[gt > 0].size / gt.size * 100
+                result[ds_name]['size'].append(percent)
+                
+                # Dice score
+                dice_score = dice(gt / 255, pred / 255) * 100
+                result[ds_name]['dice'].append(dice_score)
+
+        print("Process done!")
+        return result
+
+    @staticmethod
+    def calc_dice(name):
+        result = {
+            'CVC-300': [], 
+            'CVC-ClinicDB': [], 
+            'Kvasir': [], 
+            'CVC-ColonDB': [], 
+            'ETIS-LaribPolypDB': [],
+        }
+
+        # For each dataset
+        for ds_name in DS_NAMES:
+            print("Calc dice", ds_name)
+
+            test_raw_dataset = get_test_raw_dataset()
+            raw_result = RawResult(name)
+
+            # For each img
+            for img_idx in range(len(test_raw_dataset.filenames[ds_name])):
+
+                # Read GT, pred
+                gt = test_raw_dataset.get_gt(ds_name, img_idx)
+                pred = raw_result.get_result(ds_name, img_idx)
+
+                # Dice score
+                dice_score = dice(gt / 255, pred / 255) * 100
+                result[ds_name].append(dice_score)
+
+        print("Process done!")
+        return result
+
+    def combine(self, result):
+        new_result = result.copy()
+        new_result["all"] = {
+            "size": [],
+            "dice": []
+        }
+        for ds_name, value in result.items():
+            new_result["all"]["size"].extend(value["size"])
+            new_result["all"]["dice"].extend(value["dice"])
+        return new_result
+
+    def combine_dice(self, result):
+        new_result = result.copy()
+        new_result["all"] = []
+        for ds_name, value in result.items():
+            new_result["all"].extend(value)
+        return new_result
+
+    def show_scatter(self, name_to_compare):
+        result = self.combine(Statistic.dice_vs_size(self.name))
+        visualizer = Visualizer(NAME)
+        visualizer2 = Visualizer(name_to_compare)
+
+        def onpick(event):
+            hit_list = event.ind
+            img_idx = hit_list[0]
+            ds_name = event.artist.axes.get_title()
+            print(ds_name, img_idx)
+
+            visualizer.set_dataset_by_name(ds_name)
+            visualizer.set_image_by_index(img_idx)
+            visualizer2.set_dataset_by_name(ds_name)
+            visualizer2.set_image_by_index(img_idx)
+
+            # Show
+            frame1 = visualizer.render_frame()
+            if frame1.shape[1] > 600:
+                visualizer.scale = 0.5
+                visualizer2.scale = 0.5
+            else:
+                visualizer.scale = 1
+                visualizer2.scale = 1
+
+            frame1 = visualizer.render_frame()
+            frame2 = visualizer2.render_frame()
+            frame = np.hstack((frame1, frame2))
+
+            cv2.imshow(f"{self.name} vs {name_to_compare}", frame)
             
-            # Dice score
-            dice_score = dice(gt / 255, pred / 255) * 100
-            result[ds_name]['dice'].append(dice_score)
+        fig = plt.figure(figsize=(18, 10))
+        suptitle = fig.suptitle(f"[{self.name}] Scatter plot for dice score by polyp size", fontsize="x-large")
+        for i, (ds_name, value) in enumerate(result.items()):
+            ax = fig.add_subplot(2,3,i+1)
+            ax.scatter(value["size"], value["dice"], picker=True)
+            ax.set_title(ds_name)
+            ax.set_xlabel("Polyp size")
+            ax.set_ylabel("Dice score")
 
-    print("Process done!")
-    return result
+        fig.canvas.mpl_connect('pick_event', onpick)
+        plt.show()
 
-def combine(result):
-    new_result = result.copy()
-    new_result["all"] = {
-        "size": [],
-        "dice": []
-    }
-    for ds_name, value in result.items():
-        new_result["all"]["size"].extend(value["size"])
-        new_result["all"]["dice"].extend(value["dice"])
-    return new_result
+    def show_dice_compare(self, name_to_compare):
+        result_1 = self.combine_dice(self.calc_dice(self.name))
+        result_2 = self.combine_dice(self.calc_dice(name_to_compare))
 
-def calc_mean_and_print(result):
-    print("=====Mean dice=====")
-    for ds_name, value in result.items():
-        m = np.mean(value["dice"])
-        print(f"{ds_name}: {m:.2f} %")   
-    print("===================")
+        fig = plt.figure(figsize=(18, 10))
+        suptitle = fig.suptitle(f"Delta dice [{name_to_compare} - {self.name}]", fontsize="x-large")
+        for i, (ds_name, value) in enumerate(result_1.items()):
+            dice1 = np.array(result_1[ds_name])
+            dice2 = np.array(result_2[ds_name])
+
+            delta_dice = dice2 - dice1
+            sorted_delta = sorted(delta_dice)
+
+            ax = fig.add_subplot(2,3,i+1)
+            ax.plot(sorted_delta, marker="o")
+            ax.set_title(ds_name)
+            # ax.set_xlabel("Image")
+            ax.set_ylabel("Delta dice")
+            ax.axhline(y=0, color='r', linestyle='-')
+            
+        plt.show()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Statistic")
-    parser.add_argument("--name", type=str, default="PolypPVT")
+    parser.add_argument("--name", "-n", type=str, default="PolypPVT")
+    parser.add_argument("--compare_to", "-c", type=str, default="e_40.Dec21-15h15.cutmix.sunseg")
     opt = parser.parse_args()
 
     NAME = opt.name
+    NAME2 = opt.compare_to
     print("NAME =", NAME)
-    
-    ########## Calculate
-    result = dice_vs_size()
-    result_combined = combine(result)
-    calc_mean_and_print(result_combined)
+    print("Compare to", NAME2)
 
-    ########## Plot and visualize
-    visualizer = Visualizer(NAME)
-
-    def onpick(event):
-        hit_list = event.ind
-        img_idx = hit_list[0]
-        ds_name = event.artist.axes.get_title()
-        print(ds_name, img_idx)
-
-        visualizer.set_dataset_by_name(ds_name)
-        visualizer.set_image_by_index(img_idx)
-        visualizer.show()
-        
-    fig = plt.figure(figsize=(18, 10))
-    suptitle = fig.suptitle(f"[{NAME}] Scatter plot for dice score by polyp size", fontsize="x-large")
-    for i, (ds_name, value) in enumerate(result_combined.items()):
-        ax = fig.add_subplot(2,3,i+1)
-        ax.scatter(value["size"], value["dice"], picker=True)
-        ax.set_title(ds_name)
-        ax.set_xlabel("Polyp size")
-        ax.set_ylabel("Dice score")
-
-    fig.canvas.mpl_connect('pick_event', onpick)
-    plt.show()
-
-
+    stat = Statistic(NAME)
+    # stat.show_scatter(NAME2)
+    stat.show_dice_compare(NAME2)
--- visualize.py
+++ visualize.py
@@ -48,6 +48,8 @@ class Visualizer():
         self.test_raw_dataset = get_test_raw_dataset()
         self.raw_result = RawResult(name)
         self.set_dataset_by_index(0)
+        
+        self.frame = self.render_frame()
 
     def set_dataset_by_name(self, ds_name):
         index = DS_NAMES.index(ds_name)
@@ -62,64 +64,76 @@ class Visualizer():
     def set_image_by_index(self, index):
         self.img_idx = index % self.n_imgs
 
+    def capture(self):
+        # Make dir
+        os.makedirs(self.capture_dir, exist_ok=True)
+        # Save
+        save_path = os.path.join(self.capture_dir, f"{self.ds_name}.{self.filename}")
+        cv2.imwrite(save_path, self.frame)
+        print("Saved", save_path)
+
+    def render_frame(self):
+        # Get filename
+        self.filename = self.test_raw_dataset.filenames[self.ds_name][self.img_idx]
+
+        # Read imgs
+        img = self.test_raw_dataset.get_img(self.ds_name, self.img_idx)
+        gt = self.test_raw_dataset.get_gt(self.ds_name, self.img_idx)
+        pred = self.raw_result.get_result(self.ds_name, self.img_idx)
+
+        # Get original weight, height
+        img_height, img_width = img.shape[0], img.shape[1]
+
+        # Resize
+        img = cv2.resize(img, None, fx=self.scale, fy=self.scale)
+        gt = cv2.resize(gt, None, fx=self.scale, fy=self.scale)
+        pred = cv2.resize(pred, None, fx=self.scale, fy=self.scale)
+        
+        # Get resized image width, height
+        img_scaled_height, img_scaled_width = img.shape[0], img.shape[1]
+
+        # Frame to show
+        frame = np.copy(img)
+
+        # Blend gt
+        if self.show_gt:
+            frame = blend(frame, (255, 255, 255), gt, alpha=0.85)
+        
+        # Blend prediction
+        if self.show_pred:
+            frame = blend(frame, (0, 255, 0), pred, alpha=0.5)
+
+        # Text: dataset, image, scale, size
+        text1 = f"Dataset {self.ds_idx}: {self.ds_name}"
+        text2 = f"Image {self.img_idx}/{self.n_imgs - 1}: {self.filename}"
+        text3 = f"Scale {self.scale}x (origin size: {img_width}x{img_height})"
+        cv2.putText(img=frame, text=text1, org=(10, 40), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=self.text_color)
+        cv2.putText(img=frame, text=text2, org=(10, 60), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=self.text_color)
+        cv2.putText(img=frame, text=text3, org=(10, 80), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=self.text_color)
+
+        # Text: percent of white pixel
+        percent = gt[gt > 0].size / gt.size * 100
+        text = f"Size: {percent:.1f} %"
+        cv2.putText(img=frame, text=text, org=(10, 100), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=self.text_color)
+
+        # Text: Dice score
+        dice_score = dice(pred / 255, gt / 255) * 100
+        text = f"Dice score: {dice_score:.2f} %"
+        cv2.putText(img=frame, text=text, org=(10, 120), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=self.text_color)
+
+        # Text: Experiment name
+        text = f"Experiment name: {self.name}"
+        cv2.putText(img=frame, text=text, org=(10, img_scaled_height - 10), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=self.text_color)
+
+        return frame
+
     def show(self):
         while True:
-            # Get filename
-            filename = self.test_raw_dataset.filenames[self.ds_name][self.img_idx]
-
-            # Read imgs
-            img = self.test_raw_dataset.get_img(self.ds_name, self.img_idx)
-            gt = self.test_raw_dataset.get_gt(self.ds_name, self.img_idx)
-            pred = self.raw_result.get_result(self.ds_name, self.img_idx)
-
-            # Get original weight, height
-            img_height, img_width = img.shape[0], img.shape[1]
-
-            # Resize
-            img = cv2.resize(img, None, fx=self.scale, fy=self.scale)
-            gt = cv2.resize(gt, None, fx=self.scale, fy=self.scale)
-            pred = cv2.resize(pred, None, fx=self.scale, fy=self.scale)
-            
-            # Get resized image width, height
-            img_scaled_height, img_scaled_width = img.shape[0], img.shape[1]
-
-            # Frame to show
-            frame = np.copy(img)
-
-            # Blend gt
-            if self.show_gt:
-                frame = blend(frame, (255, 255, 255), gt, alpha=0.85)
-            
-            # Blend prediction
-            if self.show_pred:
-                frame = blend(frame, (0, 255, 0), pred, alpha=0.5)
-
-            # Text: dataset, image, scale, size
-            text1 = f"Dataset {self.ds_idx}: {self.ds_name}"
-            text2 = f"Image {self.img_idx}/{self.n_imgs - 1}: {filename}"
-            text3 = f"Scale {self.scale}x (origin size: {img_width}x{img_height})"
-            cv2.putText(img=frame, text=text1, org=(10, 40), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=self.text_color)
-            cv2.putText(img=frame, text=text2, org=(10, 60), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=self.text_color)
-            cv2.putText(img=frame, text=text3, org=(10, 80), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=self.text_color)
-
-            # Text: percent of white pixel
-            percent = gt[gt > 0].size / gt.size * 100
-            text = f"Size: {percent:.1f} %"
-            cv2.putText(img=frame, text=text, org=(10, 100), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=self.text_color)
-
-            # Text: Dice score
-            dice_score = dice(pred / 255, gt / 255) * 100
-            text = f"Dice score: {dice_score:.2f} %"
-            cv2.putText(img=frame, text=text, org=(10, 120), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=self.text_color)
-
-            # Text: Experiment name
-            text = f"Experiment name: {self.name}"
-            cv2.putText(img=frame, text=text, org=(10, img_scaled_height - 10), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=self.text_color)
-
-
             # Show
+            frame = self.render_frame()
             cv2.imshow(self.winname, frame)
 
+            # Handle event
             key = cv2.waitKey(0) & 0xFF
             if key == ord("d"):
                 self.set_image_by_index(self.img_idx + 1)
@@ -140,12 +154,7 @@ class Visualizer():
             elif key == ord("p"):
                 self.show_pred = not self.show_pred
             elif key == ord("c"): # Capture 
-                # Make dir
-                os.makedirs(self.capture_dir, exist_ok=True)
-                # Save
-                save_path = os.path.join(self.capture_dir, f"{self.ds_name}.{filename}")
-                cv2.imwrite(save_path, frame)
-                print("Saved", save_path)
+                self.capture()
             elif key == 27:
                 break
 
